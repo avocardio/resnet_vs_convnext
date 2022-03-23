ccd97a4c4e72ec3cd0010221e4f30ec4b275763c
jchburmester <64207537+jchburmester@users.noreply.github.com>
2022-03-24 00:05:56 +0100

    Some tests in train_models.py

--- Code/Final Files/scripts/train_models.py
+++ Code/Final Files/scripts/train_models.py
@@ -2,11 +2,14 @@
 
 # imports
 import argparse
+import sys
 
 # setting up CLI
 parser = argparse.ArgumentParser(description = "Training models")
 parser.add_argument("-r", "--resnet", action = "store_true", help = "starts training for resnet")
 parser.add_argument("-c", "--convnext", action = "store_true", help = "starts training for convnext")
+#parser.add_argument("-o", "--output_variable", type = int, help = "output of this script")
+
 
 args = parser.parse_args()
 
@@ -14,7 +17,9 @@ if args.resnet:
     print("training function for resnet")
     
 if args.convnext:
+    sys.argv[1] = 42
     print("training function for convnext")
+    
 
 
 # save parameters in file
--- Code/Final Files/train_convnext.sh
+++ Code/Final Files/train_convnext.sh
@@ -3,5 +3,7 @@
 # import package tfadams AdamW
 
 # train convnext
-echo "  train convnext" 
-python scripts/train_models.py --convnext
+echo "  train convnext"
+python scripts/train_models.py --convnext $1
+myvar="$1"
+echo "$myvar"
