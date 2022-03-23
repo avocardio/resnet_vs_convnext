c647a902cef69d49948c8d19c19447ada8c3f7b0
avocardio <mkalcher01@gmail.com>
2022-03-23 23:54:49 +0100

    gpu tests

--- Code/files/gpu_reader.sh
+++ Code/files/gpu_reader.sh
@@ -2,5 +2,8 @@
 
 echo "Reading GPU..."
 
-nvidia-smi --query-gpu=index,timestamp,power.draw,clocks.sm,clocks.mem,clocks.gr --format=csv -l 10 -f ./GPU-stats.csv
+nvidia-smi --query-gpu=index,timestamp,power.draw,clocks.sm,clocks.mem,clocks.gr --format=csv -l 10 -f ../../Data/GPU/GPU-stats.
 
+sleep 20
+
+./train_resnet.sh
