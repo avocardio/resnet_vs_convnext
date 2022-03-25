13d229e05c438c8d8f66fb045b03dfea151f856b
avocardio <mkalcher01@gmail.com>
2022-03-25 16:10:27 +0100

    ConvNext files

--- Code/files/scripts/train_models.py
+++ Code/files/scripts/train_models.py
@@ -28,11 +28,11 @@ if args.resnet and args.preprocessed:
     model.save(MODEL_FRAGMENTS + 'resnet50')
     
 if args.convnext and args.preprocessed:
-    train_data, _, valid_data = preprocessing_convnext()
+    train_data, _, valid_data = preprocessing_convnext(mixup=False)
     print("\nStarting training for ConvNeXt:")
     model = ConvNeXt(num_classes=400, depths=[3, 3, 9, 3], dims=[96, 192, 384, 768], include_top=True, drop_path_rate=0.1)
     model.build(input_shape=(None, 224, 224, 3))
     # need to check on optimizer
-    model.compile(optimizer=AdamW(learning_rate=0.01, weight_decay=1e-2), loss='categorical_crossentropy', metrics=['accuracy'])
+    model.compile(optimizer=AdamW(learning_rate=1e-4, weight_decay=1e-4), loss='categorical_crossentropy', metrics=['accuracy'])
     model.fit(train_data, epochs=10, validation_data=valid_data)
     model.save(MODEL_FRAGMENTS + 'convnext')
