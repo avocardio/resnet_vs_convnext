c1c94678df755c05cbfd3e7124e06f501b4d18d0
avocardio <mkalcher@uni-osnabrueck.de>
2022-03-30 11:25:51 +0200

    Fixed pipeline

--- Code/files/pipeline.sh
+++ Code/files/pipeline.sh
@@ -1,17 +1,9 @@
 #!/usr/bin/env python
 # overall pipeline for loading and preprocessing the data, running the models and and applying the GPU energy analysis
 
-echo "loading data"
-./load_data.sh
-echo "preprocessing for resnet"
-./preprocessing_resnet.sh
-echo "preprocessing for convnext"
-./preprocessing_convnext.sh
 echo "training resnet"
 ./train_resnet.sh
 echo "training convnext"
 ./train_convnext.sh
-echo "testing resnet"
-./test_resnet.sh
-echo "testing convnext"
-./test_convnext.sh
+echo "testing models"
+./test_models.sh
